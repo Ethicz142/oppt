48f10e08b4bf55bc397a872859cc64560fe66592
Ethan Nguyen <ethans142@gmail.com>
2021-05-05 15:57:31 +1000

    tiger observation plugin

---
+++ src/plugins/observationPlugins/TigerObservationPlugin/TigerObservationPlugin.cpp
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2017
+ *
+ * This file is part of On-line POMDP Planning Toolkit (OPPT).
+ * OPPT is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License published by the Free Software Foundation,
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ * OPPT is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with OPPT.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+#include "oppt/plugin/Plugin.hpp"
+
+namespace oppt
+{
+class TigerObservationPlugin: public ObservationPlugin
+{
+public :
+    TigerObservationPlugin():
+        ObservationPlugin() {
+    }
+
+    virtual bool load(const std::string& optionsFile) override {
+        debug::show_message("observation");
+        debug::show_message(optionsFile);
+        return true;
+    }
+
+    virtual ObservationResultSharedPtr getObservation(const ObservationRequest* observationRequest) const override {
+        ObservationResultSharedPtr observationResult = std::make_shared<ObservationResult>();
+        debug::show_message(debug::to_string(observationRequest));
+        return observationResult;
+    }
+
+};
+OPPT_REGISTER_OBSERVATION_PLUGIN(TigerObservationPlugin)
+}
--- src/plugins/transitionPlugins/TigerTransitionPlugin/TigerTransitionPlugin.cpp
+++ src/plugins/transitionPlugins/TigerTransitionPlugin/TigerTransitionPlugin.cpp
@@ -13,17 +13,18 @@ public:
     virtual ~TigerTransitionPlugin() = default;
 
     virtual bool load(const std::string& optionsFile) override {
+        debug::show_message("transition");
         debug::show_message(optionsFile);
         return true;
     }
 
     virtual PropagationResultSharedPtr propagateState(const PropagationRequest* propagationRequest) const override {
         PropagationResultSharedPtr propagationResult(new PropagationResult());
+        debug::show_message("propagationRequest");
+        debug::show_message(debug::to_string(propagationRequest));
         return propagationResult;
     }
 
 };
-
 OPPT_REGISTER_TRANSITION_PLUGIN(TigerTransitionPlugin)
-
 }
