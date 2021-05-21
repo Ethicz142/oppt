bbcceaf5cb26c4a96ab565b1f9fabec0a966eb16
Ethan Nguyen <ethans142@gmail.com>
2021-05-21 12:44:37 +1000

    added reward options + wrote the reward plugin

--- src/plugins/problemUtils/CuttingV2GeneralOptions.hpp
+++ src/plugins/problemUtils/CuttingV2GeneralOptions.hpp
@@ -37,6 +37,13 @@ public:
     FloatType sharpnessBound = 0.0;
     FloatType hardnessBound = 0.0;
 
+    //reward variables
+    FloatType objectCutReward = 0.0;
+    FloatType damagedPenalty = 0.0;
+    FloatType scanPenalty = 0.0;
+    FloatType cutPenalty = 0.0;
+
+
     static std::unique_ptr<options::OptionParser> makeParser() {
         std::unique_ptr<options::OptionParser> parser =
             PluginOptions::makeParser();
@@ -67,6 +74,21 @@ public:
         parser->addOption<FloatType>("observationPluginOptions",
                                         "hardnessBound",
                                         &CuttingV2GeneralOptions::hardnessBound);
+
+        //reward
+
+        parser->addOption<FloatType>("rewardPluginOptions",
+                                        "objectCutReward",
+                                        &CuttingV2GeneralOptions::objectCutReward);
+        parser->addOption<FloatType>("rewardPluginOptions",
+                                        "damagedPenalty",
+                                        &CuttingV2GeneralOptions::damagedPenalty);
+        parser->addOption<FloatType>("rewardPluginOptions",
+                                        "scanPenalty",
+                                        &CuttingV2GeneralOptions::scanPenalty);
+        parser->addOption<FloatType>("rewardPluginOptions",
+                                        "cutPenalty",
+                                        &CuttingV2GeneralOptions::cutPenalty);
     }
 };
 }
