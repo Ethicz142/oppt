0f458585f151d0b05c51e9c6761edb6c58c797a8
Ethan Nguyen <ethans142@gmail.com>
2021-05-22 23:38:50 +1000

    fixed bugs in generate script

--- tools/generateCuttingV2CFG.py
+++ tools/generateCuttingV2CFG.py
@@ -7,6 +7,7 @@ with open('modelCuttingV2.cfg', 'r') as reader:
 
 with open('CuttingV2.cfg', 'w') as writer:
   in_State = False
+  in_Action = False
   in_Observation = False
 
   for line in lines:
@@ -22,18 +23,26 @@ with open('CuttingV2.cfg', 'w') as writer:
     elif in_State and 'additionalDimensions' in line: 
       modified_line = f'additionalDimensions = {NUMBER_OF_CUTTERS * 2 + 1}\n'
     elif in_State and 'additionalDimensionLimits' in line: 
-      modified_line = 'additionalDimensionLimits = [[0, 2],' + ' [0, 1]' * NUMBER_OF_CUTTERS * 2 + ']\n'
+      modified_line = 'additionalDimensionLimits = [[0, 2]' + ', [0, 1]' * NUMBER_OF_CUTTERS * 2 + ']\n'
       in_State = False
+
+    elif in_Action and 'additionalDimensionLimits' in line: 
+      modified_line = f'additionalDimensionLimits = [[0, {NUMBER_OF_CUTTERS}]]\n'
+      in_Action = False
 
     elif in_Observation and 'additionalDimensions' in line: 
       modified_line = f'additionalDimensions = {NUMBER_OF_CUTTERS * 2 + 1}\n'
     elif in_Observation and 'additionalDimensionLimits' in line: 
-      modified_line = 'additionalDimensionLimits = [[0, 100],' + ' [0, 1]' * NUMBER_OF_CUTTERS * 2 + ']\n'
+      modified_line = 'additionalDimensionLimits = [[0, 100]' + ', [0, 1]' * NUMBER_OF_CUTTERS * 2 + ']\n'
       in_Observation = False
 
+    elif 'numInputStepsActions' in line: 
+      modified_line = f'numInputStepsActions = {NUMBER_OF_CUTTERS + 1}\n'
 
     if '[state]' in line:
       in_State = True
+    elif '[action]' in line:
+      in_Action = True
     elif '[observation]' in line:
       in_Observation = True
 
