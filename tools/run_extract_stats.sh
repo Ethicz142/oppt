08049bd4939866447bedd31ded5007f86a132291
Ethan Nguyen <ethans142@gmail.com>
2021-08-09 20:52:00 +1000

    updated the stat data for cut%

---
+++ tools/run_extract_stats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for n in 3 5 7 9 11
+do
+  for t in 1000 3000 5000
+  do
+    echo "----------------" "$n""_""2""_""$t"
+    python3 ~/Honours/oppt/tools/extract_stats.py -n $n -s 2 -t $t -i ~/Honours/oppt/runs/rs1/batch_1/"$n"_2_"$t".txt -o ~/Honours/oppt/runs/rs1/batch_1/run_results.csv
+  done
+done
+
+n=9
+for s in 9 7 5 3 1
+do
+  for t in 1000 3000 5000
+  do
+    echo "----------------" "$n""_""$s""_""$t"
+    python3 ~/Honours/oppt/tools/extract_stats.py -n $n -s $s -t $t -i ~/Honours/oppt/runs/rs1/batch_1/"$n"_"$s"_"$t".txt -o ~/Honours/oppt/runs/rs1/batch_1/run_results_2.csv
+  done
+done
