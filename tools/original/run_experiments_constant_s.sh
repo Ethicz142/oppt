6a03e4ff152b229ce3c9698e68186f9d39e94a77
Ethan Nguyen <ethans142@gmail.com>
2021-08-29 15:43:58 +1000

    segregate scripts

---
+++ tools/original/run_experiments_constant_s.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for n in 3 5 7 9 11
+do
+  for t in 1000 3000 5000
+  do
+    echo "----------------" "$n""_""2""_""$t"
+    python3 ~/Honours/oppt/tools/generateCuttingV2CFG.py -n $n -s 2 -t $t -o ~/Honours/oppt/runsexact/rs2/batch_1/cfgs/"$n"_2_"$t"-CuttingV2.cfg 
+    ~/Honours/ABTLiteOriginal/bin/./abtLite --cfg ~/Honours/oppt/runsexact/rs2/batch_1/cfgs/"$n"_2_"$t"-CuttingV2.cfg > ~/Honours/oppt/runsexact/rs2/batch_1/"$n"_2_"$t".txt
+    python3 ~/Honours/oppt/tools/extract_stats.py -n $n -s 2 -t $t -i ~/Honours/oppt/runsexact/rs2/batch_1/"$n"_2_"$t".txt -o ~/Honours/oppt/runsexact/rs2/batch_1/run_results.csv
+  done
+done
