ab5600361d5632e46967c3d79610cdf9a95cfa5a
Ethan Nguyen <ethans142@gmail.com>
2021-09-10 11:15:44 +1000

    20 and 40 test

--- tools/algo/running_for_a.sh
+++ tools/algo/running_for_a.sh
@@ -5,11 +5,11 @@ for t in 1000 3000 5000
 do
   for s in 1
   do
-    for a in 20 40 60 80
+    for a in 20 40
     do
       echo "----------------" "$n""_""$s""_""$a""_""$t"
-      python3 ~/Honours/oppt/tools/generateCuttingV2CFG.py -n $n -s $s -a $a -t $t -o ~/Honours/oppt/runsalgo/rs2/finding_a/cfgs/"$n"_"$s"_"$a"_"$t"-CuttingV2.cfg 
-      ~/Honours/ABTLite/bin/./abtLite --cfg ~/Honours/oppt/runsalgo/rs2/finding_a/cfgs/"$n"_"$s"_"$a"_"$t"-CuttingV2.cfg > ~/Honours/oppt/runsalgo/rs2/finding_a/"$n"_"$s"_"$a"_"$t".txt
+      python3 ~/Honours/oppt/tools/generateCuttingV2CFG.py -n $n -s $s -a $a -t $t -o ~/Honours/oppt/runsalgo/rs2/finding_a/batch_2/cfgs/"$n"_"$s"_"$a"_"$t"-CuttingV2.cfg 
+      ~/Honours/ABTLite/bin/./abtLite --cfg ~/Honours/oppt/runsalgo/rs2/finding_a/batch_2/cfgs/"$n"_"$s"_"$a"_"$t"-CuttingV2.cfg > ~/Honours/oppt/runsalgo/rs2/finding_a/batch_2/"$n"_"$s"_"$a"_"$t".txt
     done
   done
 done
