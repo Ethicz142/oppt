0bb8fc59735bcf64176e668fbe265d4e9b7eaf51
Ethan Nguyen <ethans142@gmail.com>
2021-09-14 18:25:17 +1000

    runs for n=20

---
+++ tools/algo/20_1_running_for_a.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+n=20
+for t in 1000 3000 5000
+do
+  for s in 1
+  do
+    for a in 20 40 50
+    do
+      echo "----------------" "$n""_""$s""_""$a""_""$t"
+      python3 ~/Honours/oppt/tools/generateCuttingV2CFG.py -n $n -s $s -a $a -t $t -o ~/Honours/oppt/runsalgo/rs3/finding_a/batch_1/cfgs/"$n"_"$s"_"$a"_"$t"-CuttingV2.cfg 
+      ~/Honours/ABTLite/bin/./abtLite --cfg ~/Honours/oppt/runsalgo/rs3/finding_a/batch_1/cfgs/"$n"_"$s"_"$a"_"$t"-CuttingV2.cfg > ~/Honours/oppt/runsalgo/rs3/finding_a/batch_1/"$n"_"$s"_"$a"_"$t".txt
+    done
+  done
+done
