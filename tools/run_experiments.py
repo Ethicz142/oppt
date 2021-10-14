22ffd18288d07b4e1b04a2ad35d0868392225587
Ethan Nguyen <ethans142@gmail.com>
2021-10-14 17:07:06 +1100

    runs for constant s

--- tools/run_experiments.py
+++ tools/run_experiments.py
@@ -74,6 +74,22 @@ if __name__ == "__main__":
     r = 50,
     trials = 20
   )
+  orig_config_constant_s = ExpConfig(
+    n_list = [5, 10, 15],
+    s_list = [1],
+    a_list = [100],
+    t_list = [1000, 3000, 5000],
+    r = 50,
+    trials = 20
+  )
+  algo_config_constant_s = ExpConfig(
+    n_list = [5, 10, 15],
+    s_list = [1],
+    a_list = [50],
+    t_list = [1000, 3000, 5000],
+    r = 50,
+    trials = 20
+  )
   # run_for_a_config = ExpConfig(
   #   n_list = [10],
   #   s_list = [1],
@@ -84,5 +100,5 @@ if __name__ == "__main__":
   # )
   # create_cfgs(orig_config, "/home/ethan/Honours/oppt/runstest")
   # generate_cutting_V2_cfg("/home/ethan/Honours/oppt/runs192/hi.cfg",3, 1, 1000, 5, 100)
-  run_experiments_original_algo(orig_config_constant_n, None, "/home/ethanNguyen/Honours/oppt/runsv1", 8)
+  run_experiments_original_algo(orig_config_constant_s, algo_config_constant_s, "/home/ethanNguyen/Honours/oppt/runsv1", 8)
 
