8054032c052a7d3597a78df0e2b61847ee2ab367
Ethan Nguyen <ethans142@gmail.com>
2021-10-23 17:38:51 +1100

    runsv3-3 setup

--- tools/run_experiments.py
+++ tools/run_experiments.py
@@ -154,6 +154,6 @@ if __name__ == "__main__":
     r = 50,
     trials = 20
   )
-  run_experiments_original_algo(orig_config_20_50_1, algo_config_20_50_1, "/home/ethanNguyen/Honours/oppt/runsv3-2", 8)
+  run_experiments_original_algo(orig_config_20_50_1, algo_config_20_50_1, "/home/ethanNguyen/Honours/oppt/runsv3-3", 8)
   print('experiments are completed.')
 
